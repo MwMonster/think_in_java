5e82c740c9aacb498d267e2d11d834b41a03311a
yaosong <yaosong@sinosoft.com.cn>
2019-03-21 14:00:57 +0800

    创建内部类

---
+++ think_in_java/src/com/mw/monster/innerclasses/Contents.java
@@ -0,0 +1,11 @@
+package com.mw.monster.innerclasses;
+
+/**
+ * @author MwMonster
+ * @Classname Contents
+ * @Description TODO
+ * @Date 2019/3/12 10:56
+ */
+public interface Contents {
+    int value();
+}
---
+++ think_in_java/src/com/mw/monster/innerclasses/Destination.java
@@ -0,0 +1,11 @@
+package com.mw.monster.innerclasses;
+
+/**
+ * @author MwMonster
+ * @Classname Destination
+ * @Description TODO
+ * @Date 2019/3/12 10:55
+ */
+public interface Destination {
+    String readLabel();
+}
